d7079e86a05725eb5d288cc1f35a6293afac24de
ravihamidov <ravihamidov42@gmail.com>
2024-12-25 13:06:21 -0800

    Add: The B of Objects Practises

---
+++ ArraysObjects/example_001.js
@@ -0,0 +1,10 @@
+let instructor = {
+  firstname: "Kelly",
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3, 4],
+};
+
+Object.keys(instructor);
+Object.values(instructor);
+Object.entries(instructor);
+instructor.hasOwnProperty("firstname");
